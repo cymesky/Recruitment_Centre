49d7cb46b14b5d6d1213e96804eeadd0a5f396ab
cymesky <cymesky@gmail.com>
2022-11-12 13:33:24 +0100

    update view for main page and link by urls

--- Recruitment_Centre/urls.py
+++ Recruitment_Centre/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from website_app import views
 
 urlpatterns = [
+    path('', views.MainPageView.as_view()),
     path('admin/', admin.site.urls),
 ]
--- website_app/views.py
+++ website_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View
+from scrapyd_api import ScrapydAPI
+from django.http import HttpResponse
+
+
+scrapyd = ScrapydAPI('http://localhost:6800')
 
 # Create your views here.
+class MainPageView(View):
+    def get(self, request):
+        task = scrapyd.schedule('default', 'icrawler')
+        return HttpResponse(task)
