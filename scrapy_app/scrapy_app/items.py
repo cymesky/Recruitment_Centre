e4d1987a0f03ab091ae6637086fb94010466332f
cymesky <cymesky@gmail.com>
2022-11-12 13:14:28 +0100

    create items for scrapy

--- scrapy_app/scrapy_app/items.py
+++ scrapy_app/scrapy_app/items.py
@@ -4,9 +4,20 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy_djangoitem import DjangoItem
+from website_app.models import PostRecruit, Recruit, Skill
 
 
 class ScrapyAppItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class PostRecruitItem(DjangoItem):
+    django_model = PostRecruit
+
+class RecruitItem(DjangoItem):
+    django_model = Recruit
+
+class SkillItem(DjangoItem):
+    django_model = Skill
