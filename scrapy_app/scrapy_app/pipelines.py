2357489568ef77f8f3acf39a6c496ec70d35162e
cymesky <cymesky@gmail.com>
2022-11-12 13:22:21 +0100

    turn on and create pipeline

--- scrapy_app/scrapy_app/pipelines.py
+++ scrapy_app/scrapy_app/pipelines.py
@@ -6,8 +6,19 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from website_app.models import PostRecruit
 
 
 class ScrapyAppPipeline:
     def process_item(self, item, spider):
+        recruit = PostRecruit(
+            post_id = item.get('post_id'),
+            post_title = item.get('post_title'),
+            post_slug = item.get('post_slug'),
+            post_replies = item.get('post_reply'),
+            post_created = item.get('post_created'),
+            post_url = item.get('post_url'),
+            post_toon_url = item.get('toon_url')
+        )
+        recruit.save()
         return item
