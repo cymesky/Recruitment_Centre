dad569559188f34ec7087b9f25ffed630363a1ca
cymesky <cymesky@gmail.com>
2022-11-12 13:35:28 +0100

    register models to admin site

--- website_app/admin.py
+++ website_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+admin.site.register(models.PostRecruit)
+admin.site.register(models.Recruit)
+admin.site.register(models.Skill)
