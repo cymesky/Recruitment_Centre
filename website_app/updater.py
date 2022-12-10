b62122a80394ff4882f61789d62b59af546d5b9f
cymesky <cymesky@gmail.com>
2022-12-10 19:30:34 +0100

    Update APScheduler settings, now first start - 15 seconds after startup and each subsequent start after one hour

--- website_app/updater.py
+++ website_app/updater.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -5,8 +6,11 @@ from .scrapy_update import update_scrapy
 
 
 def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(update_scrapy, 'interval', hours=1, jitter=120)
+    scheduler = BackgroundScheduler(timezone="Europe/Warsaw")
+    first_start_time = datetime.datetime.now() + datetime.timedelta(seconds=15)
+    scheduler.add_job(update_scrapy, 'interval', minutes=60,
+                      jitter=120, next_run_time=first_start_time)
 
     if os.environ.get('RUN_MAIN'):
         scheduler.start()
+
